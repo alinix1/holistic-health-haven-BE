3bdb7c705ac7c5a3eb173c5b2946a3be1419fa57
Ali <anix1@kent.edu>
2023-12-15 17:13:24 -0700

    add images to assets folder, create mock data

---
+++ holisticProductData.js
@@ -0,0 +1,108 @@
+let holisticProductData = [
+  {
+    id: 1,
+    product_type: ["immune system support", "anti-inflammatory"],
+    product_title: "Vitamin C",
+    img: "../assets/vitamin_c.jpg",
+    product_description:
+      "describe overall product, quantity, recommended dose per serving, potential side effects",
+    price: 24.99,
+  },
+  {
+    id: 2,
+    product_type: ["anti-inflammatory", "de-stress", "muscle relaxation"],
+    product_title: "CBD salve",
+    img: "../assets/cbd_salve.jpg",
+    product_description:
+      "describe overall product, quantity, recommended dose per serving, potential side effects",
+    price: 30.0,
+  },
+  {
+    id: 3,
+    product_type: ["de-stress", "muscle relaxation"],
+    product_title: "Epsom salts",
+    img: "../assets/epsom_salts.jpg",
+    product_description:
+      "describe overall product, quantity, recommended dose per serving, potential side effects",
+    price: 15.99,
+  },
+  {
+    id: 4,
+    product_type: ["heart health", "muscle relaxation"],
+    product_title: "Potassium",
+    img: "../assets/potassium.jpg",
+    product_description:
+      "describe overall product, quantity, recommended dose per serving, potential side effects",
+    price: 10.5,
+  },
+  {
+    id: 5,
+    product_type: "Melatonin",
+    product_title: ["sleep support"],
+    img: "../assets/melatonin.jpg",
+    product_description:
+      "describe overall product, quantity, recommended dose per serving, potential side effects",
+    price: 12.5,
+  },
+  {
+    id: 6,
+    product_type: ["de-stress", "immune system support", "anti-inflammatory"],
+    product_title: "Ashwagandha",
+    img: "../assets/ashwagandha.jpg",
+    product_description:
+      "describe overall product, quantity, recommended dose per serving, potential side effects",
+    price: 17.99,
+  },
+  {
+    id: 7,
+    product_type: ["anti-inflammatory", "immune system support"],
+    product_title: "Turmeric",
+    img: "../assets/turmeric.jpg",
+    product_description:
+      "describe overall product, quantity, recommended dose per serving, potential side effects",
+    price: 12.5,
+  },
+  {
+    id: 8,
+    product_type: [
+      "liver cleanse",
+      "anti-inflammatory",
+      "immune system support",
+    ],
+    product_title: "Milk thistle",
+    img: "../assets/milk_thistle.jpg",
+    product_description:
+      "describe overall product, quantity, recommended dose per serving, potential side effects",
+    price: 10.99,
+  },
+  {
+    id: 9,
+    product_type: ["immune system support", "gut health"],
+    product_title: "Probiotic",
+    img: "../assets/probiotics.jpg",
+    product_description:
+      "describe overall product, quantity, recommended dose per serving, potential side effects",
+    price: 39.99,
+  },
+  {
+    id: 10,
+    product_type: ["anti-microbial", "wound healing"],
+    product_title: "Tea tree oil",
+    img: "../assets/tea_tree_oil.jpg",
+    product_description:
+      "describe overall product, quantity, recommended dose per serving, potential side effects",
+    price: 7.5,
+  },
+  {
+    id: 11,
+    product_type: ["energy support"],
+    product_title: "Vitamin B12",
+    img: "../assets/vitamin_b12.jpg",
+    product_description:
+      "describe overall product, quantity, recommended dose per serving, potential side effects",
+    price: 15.0,
+  },
+];
+
+//module.exports = holisticProductData
+export default holisticProductData;
