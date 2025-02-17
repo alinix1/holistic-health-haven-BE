1063fbcbb82f2a75ee08621c60ca3eee516bfc76
alinix1 <anix1@kent.edu>
2025-02-16 19:05:27 -0700

    replaces mock data with database queries in products and reviews routes

--- routes/products/server.js
+++ routes/products/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const mockProducts = require("./mockData/productsData.js");
+const database = require("../../db/knex");
 
 /* --------------GET ALL PRODUCTS------------- */
 
 router.get("/", async (request, response) => {
   try {
-    response.status(200).json(mockProducts);
+    const products = await database("products").select();
+    response.status(200).json(products);
   } catch (error) {
     console.error(error);
     response.status(500).json({ error: "Server error" });
@@ -18,8 +19,9 @@ router.get("/", async (request, response) => {
 router.get("/:id", async (request, response) => {
   try {
     const productId = Number.parseInt(request.params.id, 10);
-    const product = mockProducts.find((item) => item.id === productId);
-    if (product.length) {
+    const product = await database("products").where("id", productId).first();
+
+    if (product) {
       response.status(200).json(product);
     } else {
       response.status(404).json({ error: "Product not found" });
--- routes/reviews/server.js
+++ routes/reviews/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const mockReviews = require("./mockData/reviewsData.js");
+const database = require("../../db/knex");
 
 /* --------------GET ALL REVIEWS BY CUSTOMERS------------- */
 
 router.get("/", async (request, response) => {
   try {
-    response.status(200).json(mockReviews);
+    const reviews = await database("reviews").select();
+    response.status(200).json(reviews);
   } catch (error) {
     console.error(error);
     response.status(500).json({ error: "Server error" });
@@ -16,15 +17,14 @@ router.get("/", async (request, response) => {
 /* --------------POST A NEW REVIEW------------- */
 
 router.post("/", async (request, response) => {
-  const newReview = request.body;
+  const review = request.body;
 
   for (let requiredParameter of [
-    "id",
     "holistic_product_id",
     "user_name",
     "user_review",
   ]) {
-    if (!newReview[requiredParameter]) {
+    if (!review[requiredParameter]) {
       return response
         .status(422)
         .json({ error: `Missing ${requiredParameter} in request body` });
@@ -32,8 +32,8 @@ router.post("/", async (request, response) => {
   }
 
   try {
-    mockReviews.push(newReview);
-    response.status(201).json(newReview);
+    const id = await database("reviews").insert(review, "id");
+    response.status(201).json(id);
   } catch (error) {
     console.error(error);
     response.status(500).json({ error: "Server error" });
