4d4fef637adf0a4789fbb537b84fa4788963a5d7
alinix1 <anix1@kent.edu>
2025-02-18 16:23:08 -0700

    add stripe routes for payment processing

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const reviewsRoutes = require("./reviews/server");
 const productsRoutes = require("./products/server");
 const statesRoutes = require("./states/server");
+const stripeRoutes = require("./stripe/server");
 
 // mount specific routes
 router.use("/reviews", reviewsRoutes);
 router.use("/products", productsRoutes);
 router.use("/states", statesRoutes);
+router.use("/stripe", stripeRoutes);
 
 module.exports = router;
